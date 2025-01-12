e83b59312d308e7616b6c4b87f811bfa2099fceb
GeorgiStoyanov05 <g.stoyanov05@gmail.com>
2025-01-12 19:47:19 +0200

    Added the text that shows all of the options to the user

--- Restaurant Project/Restaurant Project/Restaurant Project.cpp
+++ Restaurant Project/Restaurant Project/Restaurant Project.cpp
@@ -27,22 +27,53 @@ static bool compareStrings(const char* str1, const char* str2) {
 	return areEqual;
 }
 
+static void DisplayUserOptions(const char* role) {
+	if (compareStrings(role, "Waiter"))
+	{
+		std::cout << "1) See the menu" << std::endl;
+		std::cout << "2) Order something from the menu" << std::endl;
+		std::cout << "3) Cancel an order" << std::endl;
+		std::cout << "4) See all previous orders" << std::endl;
+		std::cout << "5) See all previous orders, sorted in alphabetical order, as well as the number of orders of each dish" << std::endl;
+		std::cout << "6) See today's turnover" << std::endl;
+		std::cout << "7) Show all options again" << std::endl;
+	}
+	else if (compareStrings(role, "Manager"))
+	{
+		std::cout << "1) See the menu" << std::endl;
+		std::cout << "2) Order something from the menu" << std::endl;
+		std::cout << "3) Cancel an order" << std::endl;
+		std::cout << "4) See all previous orders" << std::endl;
+		std::cout << "5) See all previous orders, sorted in alphabetical order, as well as the number of orders of each dish" << std::endl;
+		std::cout << "6) See the quantity for all the remaining products" << std::endl;
+		std::cout << "7) Remove a product from the storage" << std::endl;
+		std::cout << "8) Add a product to the storage" << std::endl;
+		std::cout << "9) See today's turnover" << std::endl;
+		std::cout << "10) Generate today's report" << std::endl;
+		std::cout << "11) See all the turnovers from a certain day to today" << std::endl;
+		std::cout << "12) Add a dish to the Menu" << std::endl;
+		std::cout << "13) Remove a dish from the menu" << std::endl;
+		std::cout << "14) Show all options again" << std::endl;
+	}
+}
+
 int main()
 {
 	char role[8];
 	std::cout << "Select a role: ";
 	std::cin >> role;
+
 	while (compareStrings(role, "Manager")==0 && compareStrings(role, "Waiter")==0)
 	{
-		std::cout << "This role was invalid! Please select another one: ";
+		std::cout << "This role was invalid! Please select another one (Waiter or Manager): ";
 		std::cin >> role;
 	}
+
+	DisplayUserOptions(role);
 
 	struct Student {
 		char name[45];
 		int age;
 		int heigth;
 	};
-
-	std::cout << getStringLenght(role);
 }
