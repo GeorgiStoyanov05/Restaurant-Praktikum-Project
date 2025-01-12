0a368b89596146af30eecb42ddbc0b2676bb9647
GeorgiStoyanov05 <g.stoyanov05@gmail.com>
2025-01-12 19:09:39 +0200

    Created a Length and a Compare method
    Created two methods - one that compares two strings and returns a bool value that indicated whether they are equal or not and also another one that returns a string's length.

--- Restaurant Project/Restaurant Project/Restaurant Project.cpp
+++ Restaurant Project/Restaurant Project/Restaurant Project.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
 
+static int getStringLenght(const char* str)
+{
+	int length = 0;
+	while (str[length] != '\0') {
+		length++;
+	}
+	return length;
+}
+
+static bool compareStrings(const char* str1, const char* str2) {
+	int strLenght1 = getStringLenght(str1);
+	int strLenght2 = getStringLenght(str2);
+	bool areEqual = 1;
+	if (strLenght1 != strLenght2) {
+		return 0;
+	}
+	else {
+		for (int i = 0; i < strLenght1; i++) {
+			if (str1[i] != str2[i])
+			{
+				areEqual = 0;
+			}
+		}
+	}
+	return areEqual;
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	char role[8];
+	std::cin >> role;
+
+	std::cout << role << std::endl;
+
+	struct Student {
+		char name[45];
+		int age;
+		int heigth;
+	};
+
+	std::cout << getStringLenght(role);
 }
